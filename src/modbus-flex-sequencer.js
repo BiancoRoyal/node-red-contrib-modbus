3ce022f14c9df3c0c9242ef920a44688098d677a
Andrea Verardi <62706586+anversoft@users.noreply.github.com>
2021-04-22 11:34:40 +0200

    Updated The BSD 3-Clause License

--- src/modbus-flex-sequencer.js
+++ src/modbus-flex-sequencer.js
@@ -3,7 +3,7 @@
  All rights reserved.
  node-red-contrib-modbus - The BSD 3-Clause License
 
- @author <a href="mailto:klaus.landsdorf@bianco-royal.de">Klaus Landsdorf</a> (Bianco Royal)
+@author <a>Andrea Verardi</a> (Anversoft)
  */
 /**
  * Modbus Getter node.
