fad930b841d512c6f37574831a2c00f673c39b38
Klaus Landsdorf <kl@iniationware.com>
2022-04-25 10:55:03 +0200

    test: coverage next level

--- clean.sh
+++ clean.sh
@@ -14,16 +14,24 @@ rm -rf docs/gen
 
 rm package-lock.json
 
+rm yarn.lock
+
 npm cache verify
 
 npm install
 
 npm i --only=dev
 
+yarn
+
 npm test
+
+npm run test-with-coverage
 
 npm run build
 
 npm run rewrite-changelog
 
 node -v
+
+npm audit
