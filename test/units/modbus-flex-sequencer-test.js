b8c12b94e8f3e485348b462d1a0f0cd031a19723
ckiniation <ck@iniationware.com>
2022-11-08 10:23:14 +0100

    feat(flex-sequencer): extracted and updated flows

--- test/units/modbus-flex-sequencer-test.js
+++ test/units/modbus-flex-sequencer-test.js
@@ -20,6 +20,8 @@ helper.init(require.resolve('node-red'))
 
 var testFlexSequencerNodes = [injectNode, clientNode, serverNode, nodeUnderTest]
 
+var testFlows = require('./flows/modbus-flex-sequencer-flows')
+
 describe('Flex Sequencer node Testing', function () {
   before(function (done) {
     helper.startServer(function () {
@@ -43,23 +45,7 @@ describe('Flex Sequencer node Testing', function () {
 
   describe('Node', function () {
     it('simple Node should be loaded without client config', function (done) {
-      helper.load([nodeUnderTest], [{
-        id: 'bc5a61b6.a3972',
-        type: 'modbus-flex-sequencer',
-        name: 'modbusFlexSequencer',
-        showStatusActivities: false,
-        showErrors: false,
-        server: '',
-        useIOFile: false,
-        ioFile: '',
-        useIOForPayload: false,
-        emptyMsgOnFail: false,
-        keepMsgProperties: false,
-        wires: [
-          [],
-          []
-        ]
-      }], function () {
+      helper.load(testFlexSequencerNodes,testFlows.testNodeWithoutClientFlow , function () {
         const modbusFlexSequencer = helper.getNode('bc5a61b6.a3972')
         modbusFlexSequencer.should.have.property('name', 'modbusFlexSequencer')
 
@@ -70,64 +56,7 @@ describe('Flex Sequencer node Testing', function () {
     })
 
     it('simple Node with server should be loaded', function (done) {
-      helper.load([clientNode, serverNode, nodeUnderTest], [{
-        id: 'bc5a61b6.a3972',
-        type: 'modbus-flex-sequencer',
-        name: 'modbusFlexSequencer',
-        showStatusActivities: false,
-        showErrors: false,
-        server: '92e7bf63.2efd7',
-        useIOFile: false,
-        ioFile: '',
-        useIOForPayload: false,
-        emptyMsgOnFail: false,
-        keepMsgProperties: false,
-        wires: [
-          [],
-          []
-        ]
-      }, {
-        id: '996023fe.ea04b',
-        type: 'modbus-server',
-        name: 'modbusServer',
-        logEnabled: true,
-        hostname: '127.0.0.1',
-        serverPort: '7506',
-        responseDelay: 100,
-        delayUnit: 'ms',
-        coilsBufferSize: 10000,
-        holdingBufferSize: 10000,
-        inputBufferSize: 10000,
-        discreteBufferSize: 10000,
-        showErrors: false,
-        wires: [
-          [],
-          [],
-          []
-        ]
-      }, {
-        id: '92e7bf63.2efd7',
-        type: 'modbus-client',
-        name: 'ModbusServer',
-        clienttype: 'tcp',
-        bufferCommands: true,
-        stateLogEnabled: true,
-        parallelUnitIdsAllowed: true,
-        tcpHost: '127.0.0.1',
-        tcpPort: '7506',
-        tcpType: 'DEFAULT',
-        serialPort: '/dev/ttyUSB',
-        serialType: 'RTU-BUFFERD',
-        serialBaudrate: '9600',
-        serialDatabits: '8',
-        serialStopbits: '1',
-        serialParity: 'none',
-        serialConnectionDelay: '100',
-        unit_id: '1',
-        commandDelay: '1',
-        clientTimeout: '100',
-        reconnectTimeout: 200
-      }], function () {
+      helper.load(testFlexSequencerNodes,testFlows.testNodeWithServerFlow , function () {
         const modbusServer = helper.getNode('996023fe.ea04b')
         modbusServer.should.have.property('name', 'modbusServer')
 
