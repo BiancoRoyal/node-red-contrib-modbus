94d857188451ce36e990affbc34ba6574ced6e4d
Steffen Beyer <steffen@beyer.io>
2024-01-29 09:59:51 +0100

    fix(package): remove deps on sh and node in path

--- supporter.js
+++
@@ -1,3 +0,0 @@
-const message =
-  '\u001b[96m\u001b[1mThank you for using our contribution!\u001b[96m\u001b[1m\n\u001b[0m\u001b[96mIf you rely on this package, please consider supporting our open source work:\u001b[22m\u001b[39m\n> \u001b[94mhttps://bianco-royal.space/supporter/\u001b[0m\n\n'
-console.log(message)
