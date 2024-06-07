06fcdce77988b17db6a8ac67f9a3403c4211e335
mj <maryam.jalil@iniationware.com>
2024-06-07 20:45:22 +0200

    test(modbus-flex): added test case to process valid sequences

--- src/modbus-flex-sequencer.js
+++ src/modbus-flex-sequencer.js
@@ -198,6 +198,7 @@ module.exports = function (RED) {
     node.initializeInputDelayTimer()
 
     node.on('input', (msg) => {
+      console.log(msg, 'line909')
       /* istanbul ignore next */
       if (mbBasics.invalidPayloadIn(msg)) {
         verboseWarn('Invalid message on input.')
--- test/e2e/modbus-flex-sequencer-e2e-test.js
+++ test/e2e/modbus-flex-sequencer-e2e-test.js
@@ -17,16 +17,14 @@ const nodeUnderTest = require('../../src/modbus-flex-sequencer.js')
 
 const helper = require('node-red-node-test-helper')
 helper.init(require.resolve('node-red'))
-// const assert=require('assert')
 const testFlexSequencerNodes = [injectNode, clientNode, serverNode, nodeUnderTest]
+// const mbBasics = require('../../src/modbus-basics.js')
 
 const testFlows = require('./flows/modbus-flex-sequencer-e2e-flows.js')
-// const mBasics = require('../../src/modbus-basics')
-// const _ = require('underscore')
-
-// const chai = require('chai')
+// const assert = require('assert')
 const sinon = require('sinon')
-// const expect = chai.expect
+const chai = require('chai')
+const expect = chai.expect
 
 describe('Flex Sequencer node Testing', function () {
   before(function (done) {
@@ -50,6 +48,54 @@ describe('Flex Sequencer node Testing', function () {
   })
 
   describe('Node', function () {
+    // it('should handle error in input message processing', function () {
+    //   helper.load(testFlexSequencerNodes, testFlows.testNodeResponseFromServer, () => {
+    //     const flexSequencerNode = helper.getNode('bae63bd33cee1ff2')
+    //     console.log(flexSequencerNode)
+    //     const origMsgInput = { sequences: [{ fc: 'FC1', unitid: '1', address: '10', quantity: '2' }] }
+    //     // const error = new Error('Error processing input message')
+
+    //     // flexSequencerNode.isValidModbusMsg = function () {
+    //     //   throw error
+    //     // }
+
+    //     flexSequencerNode.emit('input', origMsgInput)
+
+    //     //   expect(flexSequencerNode.errorProtocolMsg).toHaveBeenCalledTimes(1)
+    //     //   expect(flexSequencerNode.errorProtocolMsg).toHaveBeenCalledWith(error, origMsgInput)
+
+    //   //   expect(mbBasics.sendEmptyMsgOnFail).toHaveBeenCalledTimes(1)
+    //   //   expect(mbBasics.sendEmptyMsgOnFail).toHaveBeenCalledWith(flexSequencerNode, error, origMsgInput)
+    //   })
+    // })
+
+    it('should process valid sequences', function (done) {
+      helper.load(testFlexSequencerNodes, testFlows.testNodeWithValidSequence, () => {
+        const flexSequencerNode = helper.getNode('607b91b18be2a9ee')
+        const msg = {
+          payload: 'test payload',
+          sequences: [
+            {
+              unitid: 1,
+              fc: 'FC3',
+              address: 0,
+              quantity: 10
+            }
+          ]
+        }
+        let setStatus = {}
+
+        flexSequencerNode.status = function (status) {
+          setStatus = status
+        }
+        setTimeout(function () {
+          flexSequencerNode.emit('input', msg)
+          expect(setStatus).to.deep.equal({ fill: 'green', shape: 'ring', text: 'connected' })
+          done()
+        }, 1500)
+      })
+    })
+
     it('should handle modbus read error', function (done) {
       helper.load(testFlexSequencerNodes, testFlows.testNodeWithModbusReadError, () => {
         const flexSequencerNode = helper.getNode('bc5a61b6.a3972')
