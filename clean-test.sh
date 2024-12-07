5a26a2fe3b5a446b7be17c150a15516df4ca90b7
Klaus Landsdorf <klaus.landsdorf@iniationware.com>
2024-12-07 11:37:42 +0100

    test(mocha):  new  settings for Mocha and Intellij

---
+++ clean-test.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+node -v
+
+rm -rf node_modules/
+
+rm -rf modbus/
+
+rm -rf code/
+
+rm -rf .nyc_output/
+
+rm -rf coverage/
+
+rm -rf docs/gen
+
+rm package-lock.json
+
+rm yarn.lock
+
+npm cache verify
+
+npm install
+
+npm i --only=dev
+
+yarn
+
+npm test
