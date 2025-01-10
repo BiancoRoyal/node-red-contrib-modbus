95c83f4591b0576faaa30dceda01b7e19811d31f
Kay Grewe <kg@iniationware.com>
2025-01-10 13:55:18 +0100

    test: import first test from old test suite, start of improving it

--- __tests__/units/modbus-client.test.js
+++ __tests__/units/modbus-client.test.js
@@ -1,4 +1,3 @@
-
 const serverNode = require('../../src/modbus-server.js')
 const nodeUnderTest = require('../../src/modbus-client.js')
 const readNode = require('../../src/modbus-read.js')
@@ -6,20 +5,38 @@ const flexGetterNode = require('../../src/modbus-flex-getter.js')
 
 const testModbusClientNodes = [serverNode, nodeUnderTest, readNode, flexGetterNode]
 const helper = require('node-red-node-test-helper')
+const testFlows = require('./flows/modbus-client-flows.js')
+
+
+
 describe('Client Node Unit Testing', () => {
-  beforeAll(() => {
-    console.log("Preparing for tests")
+  beforeAll((done) => {
+    helper.startServer(done)
   })
 
   afterEach(() => {
-    console.log("Preparing cleanup after test")
+    jest.useRealTimers()
   })
 
-  afterAll(() => {
-    console.log("Executing cleanup after test suite")
+  afterAll((done) => {
+    helper.unload().then(function() {
+      helper.stopServer(done)
+    })
   })
 
-  test("should handle error and log warning on deregister node for modbus", () => {
-    expect(true).toBe(true)
+  test("deregister modbus get's called with the correct id", (done) => {
+    helper.load(testModbusClientNodes, testFlows.testClientWithoutServerFlow, () => {
+      jest.useFakeTimers()
+      const modbusClientNode = helper.getNode('3')
+      const clientUserNodeId = 'clientUserNodeId'
+
+      modbusClientNode.registeredNodeList[clientUserNodeId] = true
+
+      //jest.spyOn(modbusClientNode, 'closeConnectionWithoutRegisteredNodes')
+      jest.spyOn(modbusClientNode, 'deregisterForModbus').mockImplementation((innerId, data) => {
+        expect(innerId).toBe('clientUserNodeId')
+      })
+      done()
+    })
   })
 })
